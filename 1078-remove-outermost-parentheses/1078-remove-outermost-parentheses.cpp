735757ae9d79be8834ec51b87e7fc44324a7d6cf
Siddhant Mishra <123siddhantm@gmail.com>
2025-09-09 17:52:47 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.49%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.length();
+        string ans ;
+        int cnt = 0;
+        for(int i=0 ; i<n;++i)
+        {
+            if(s[i]==')') cnt--;
+            if ( cnt != 0) ans += s[i] ;
+            if ( s[i] == '(') cnt++;
+        }
+        return ans;
+
+    }
+};
